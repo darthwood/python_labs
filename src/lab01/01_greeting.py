d2221b7eee2bb01ffa1b6b80e1b760bace41bd71
darthwood <dronasbis@yandex.ru>
2025-09-21 16:54:57 +0300

    news1

--- src/lab01/01_greeting.py
+++ src/lab01/01_greeting.py
@@ -1,2 +1,8 @@
 a,b = input('Имя:'), int(input('Возраст:'))
 print(f'Привет {a}, через год тебе будет {b+1}.')
+
+# src/lab01/01_greeting.py
+# Имя:Андрей
+# Возраст:31
+# Привет Андрей, через год тебе будет 32.
+# PS C:\Users\Andrew\HiGIT\python_labs> 
