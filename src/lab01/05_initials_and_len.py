a094ba8f1a2f02a018e6ceaf91bb4b9187cfb9c2
darthwood <dronasbis@yandex.ru>
2025-09-23 12:10:29 +0300

    ex5

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,15 +1,17 @@
 S = input()
-M = S.strip()
-M = M.replace('  ','')
-k = len(M)
+M = S.strip().split()
+L = ' '.join(M)
+k = len(L)
 F = ''
 for i in S:
     if i.isupper():
         F += i
+print(L)
 print(f'Инициалы: {F}')
 print(f'Длина (символов): {k}')
 
 # src/lab01/05_initials_and_len.py
-#       Иванов Иван Иванович
+#    Иванов   Иван   Иванович
+# Иванов Иван Иванович
 # Инициалы: ИИИ
 # Длина (символов): 20
