ed8145536779f33c1dc4f01b49c1598f854a5b8a
darthwood <dronasbis@yandex.ru>
2025-09-23 11:47:14 +0300

    ex2

--- src/lab01/02_sum_avg.py
+++ src/lab01/02_sum_avg.py
@@ -1,7 +1,7 @@
-a,b = float(input()), float(input())
+a,b = float(input().replace(',','.')), float(input().replace(',','.'))
 print(f'sum={(a+b):.2f}; avg={((a+b)/2):.2f}')
 
 # src/lab01/02_sum_avg.py
-# 3.5
+# 3,5
 # 4.25
 # sum=7.75; avg=3.88
