dbc8f612947724ebdef0d52e231c16fea89a452a
darthwood <dronasbis@yandex.ru>
2025-10-04 18:12:17 +0300

    lbs3

---
+++ src/lab02/flattern.py
@@ -0,0 +1,21 @@
+def flattern(n):
+    a = list()
+    for i in n:
+        if i.isdigit():
+            a.append(i)
+        if i.isalpha():
+            return 'TypeError'
+    return a
+
+n = list(map(str, input()))
+print(flattern(n))
+
+# src/lab02/flattern.py
+# [[1, 2], [3, 4]]
+# ['1', '2', '3', '4']
+# [[1, 2], (3, 4, 5)]
+# ['1', '2', '3', '4', '5']
+# [[1], [], [2, 3]]
+# ['1', '2', '3']
+# [[1, 2], "ab"]
+# TypeError
