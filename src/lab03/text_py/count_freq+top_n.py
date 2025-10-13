36129ab8523127b93fc9c2036887db007d4d7d50
darthwood <dronasbis@yandex.ru>
2025-10-13 16:12:36 +0300

    swag

---
+++ src/lab03/text_py/count_freq+top_n.py
@@ -0,0 +1,24 @@
+c1 = ["a","b","a","c","b","a"]
+c2 = ["bb","aa","bb","aa","cc"]
+
+def count_freq(n):
+    s = set(n)
+    s = sorted((s))
+    my_dic = {}
+    for i in s:
+        my_dic[i] = n.count(i)
+    return my_dic
+
+def top_n(n):
+    s = count_freq(n)
+    return list(s.items())[:2]
+
+print(count_freq(c1))
+print(count_freq(c2))
+print(top_n(c1))
+print(top_n(c2))
+
+# {'a': 3, 'b': 2, 'c': 1}
+# {'aa': 2, 'bb': 2, 'cc': 1}
+# [('a', 3), ('b', 2)]
+# [('aa', 2), ('bb', 2)]
