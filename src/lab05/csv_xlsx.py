efd77a80f1ea2b32ad4f5f61a8e589bf999076b6
darthwood <dronasbis@yandex.ru>
2025-11-16 16:24:33 +0300

    brokenglass

--- src/lab05/csv_xlsx.py
+++ src/lab05/csv_xlsx.py
@@ -2,17 +2,23 @@ from pathlib import Path
 import csv 
 from openpyxl import Workbook
 
-def csv_2_xlsx(puta: str|Path, encoding='utf-8'):
+def csv_2_xlsx(puta: str|Path, outputa: str|Path = None, encoding='utf-8'):
     nig = Workbook()
     lis = nig.active
     lis.title = 'Девятихвостый Лис'
+
+    if outputa is None:
+        puta = Path(puta)
+        diroputa = Path('data\output_stuff')
+        outputa = diroputa / f'{puta.stem}.xlsx'
+
 
     with open(puta,'r',encoding='utf-8') as csv_fi:
         red = csv.reader(csv_fi)
         for nu_st, lin in enumerate(red,1):
             for nu_cmn, val in enumerate(lin,1):
                 lis.cell(row=nu_st,column=nu_cmn,value=val)
-    nig.save('src\lab05\ltabla.xlsx')  
+    nig.save(outputa)  
 
 
 an = csv_2_xlsx('src\lab05\pupa.csv',encoding='utf-8') 
